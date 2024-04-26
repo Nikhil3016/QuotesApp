b87e98a330c20ae2e60a3e3e022b640eb9c9491f
Nikhil <nikhilkumaraajjo@gmail.com>
2024-04-26 18:00:43 +0530

    Initial commit

--- app/src/main/java/com/nk/quotes/MainActivity.kt
+++ app/src/main/java/com/nk/quotes/MainActivity.kt
@@ -4,18 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.nk.quotes.screens.QuoteListItem
 import com.nk.quotes.screens.QuoteListScreen
 import com.nk.quotes.ui.theme.QuotesTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        DataManager.loadAssets(this)
+        CoroutineScope(Dispatchers.IO).launch {
+            DataManager.loadAssets(this@MainActivity)
+        }
+
 
         setContent {
             QuotesTheme {
@@ -24,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    App()
                 }
             }
         }
@@ -34,8 +43,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     if (DataManager.isDataLoaded.value) {
-        QuoteListScreen(quotes = DataManager.data) {
+        LazyColumn {
+            items(DataManager.data) {
+                QuoteListItem(it) {
 
+                }
+            }
         }
     }
 }
